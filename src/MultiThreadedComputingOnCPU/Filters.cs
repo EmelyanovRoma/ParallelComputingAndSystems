adbb4b1ab23e8f12a36177ed85e6bcb58397ab87
Romanter456@gmail.com <Romanter456@gmail.com>
2024-10-04 02:05:50 +0700

    comments(Filters.cs): добавлены xml-комментарии для класса Filters и Erosion

--- src/MultiThreadedComputingOnCPU/Filters.cs
+++ src/MultiThreadedComputingOnCPU/Filters.cs
@@ -6,32 +6,51 @@ using System.Threading.Tasks;
 
 namespace MultiThreadedComputingOnCPU
 {
+    /// <summary>
+    /// Описывает морфологичесикий фильтр эрозии и
+    /// фильтр размытия со смещением пикселей изображения.
+    /// </summary>
     public static class Filters
     {
+        /// <summary>
+        /// Задает или возвращает количество потоков процессора.
+        /// </summary>
         public static int Threads { set; get; }
 
-
+        /// <summary>
+        /// Описывает фильтр эрозии.
+        /// </summary>
         public static class Erosion
-        {          
-            public static double[,] CalculateIntensivityOfImage(Bitmap bitmap)
+        {
+            /// <summary>
+            /// Вычисляет интенсивность пикселей изображения.
+            /// </summary>
+            /// <param name="image">Исходное изображение.</param>
+            /// <returns>Матрица интенсивности пикселей.</returns>
+            public static double[,] CalculateIntensivityOfImage(Bitmap image)
             {
-                BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height),
-                                                    ImageLockMode.ReadWrite, bitmap.PixelFormat);
-                int bytesPerPixel = Image.GetPixelFormatSize(bitmap.PixelFormat) / 8;
-                int byteCount = bitmapData.Stride * bitmap.Height;
+                BitmapData bitmapData = image.LockBits(
+                    new Rectangle(0, 0, image.Width, image.Height),
+                    ImageLockMode.ReadWrite,
+                    image.PixelFormat);
+                
+                int bytesPerPixel = Image.GetPixelFormatSize(image.PixelFormat) / 8;
+                int byteCount = bitmapData.Stride * image.Height;
                 byte[] pixels = new byte[byteCount];
-
+                double[,] intensivityMatrixImage = new double[image.Width, image.Height];                
+                int widtn = image.Width;
                 IntPtr ptrFirstPixel = bitmapData.Scan0;
-                Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length);
 
-                int height = bitmap.Height;
-                int width = bitmap.Width;
-                double[,] intensivityMatrixImage = new double[width, height];
-
-                Parallel.For(0, height, new ParallelOptions { MaxDegreeOfParallelism = Threads }, y =>
+                Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length); 
+                
+                Parallel.For(
+                    0,
+                    image.Height,
+                    new ParallelOptions { MaxDegreeOfParallelism = Threads },
+                    y =>
                 {
                     int yIndex = y * bitmapData.Stride;
-                    for (int x = 0; x < width; x++)
+                    for (int x = 0; x < widtn; x++)
                     {
                         int xIndex = x * bytesPerPixel;
                         int index = yIndex + xIndex;
@@ -46,44 +65,66 @@ namespace MultiThreadedComputingOnCPU
                 });
 
                 Marshal.Copy(pixels, 0, ptrFirstPixel, pixels.Length);
-                bitmap.UnlockBits(bitmapData);
+                image.UnlockBits(bitmapData);
 
                 return intensivityMatrixImage;
             }
 
-            private static int[,] ApplyThreshold(double[,] intensivity, int threshold)
+            /// <summary>
+            /// Применяет значение порога для формирования матрицы из 0 и 1.
+            /// </summary>
+            /// <param name="intensivityMat">Матрица интенсивности пикселей.</param>
+            /// <param name="threshold">Значение порога.</param>
+            /// <returns>Матрица 0 и 1 (бинарное изображение).</returns>
+            private static int[,] ApplyThreshold(double[,] intensivityMat, int threshold)
             {
-                int width = intensivity.GetLength(0);
-                int height = intensivity.GetLength(1);
+                int width = intensivityMat.GetLength(0);
+                int height = intensivityMat.GetLength(1);
                 var binaryImage = new int[width, height];
 
-                Parallel.For(0, width, new ParallelOptions { MaxDegreeOfParallelism = Threads }, x =>
+                Parallel.For(
+                    0,
+                    width,
+                    new ParallelOptions { MaxDegreeOfParallelism = Threads },
+                    x =>
                 {
                     for (int y = 0; y < height; y++)
                     {
-                        binaryImage[x, y] = intensivity[x, y] < threshold ? 0 : 1;
+                        binaryImage[x, y] = intensivityMat[x, y] < threshold ? 0 : 1;
                     }
                 });
 
                 return binaryImage;
             }
 
+            /// <summary>
+            /// Конвертирует бинарное изображение в <see cref="Bitmap"/>.
+            /// </summary>
+            /// <param name="binaryImage">Бинарное изображение.</param>
+            /// <returns>24-битное изображение.</returns>
             private static Bitmap ConvertBinaryImageToBitmap(int[,] binaryImage)
             {
                 int width = binaryImage.GetLength(0);
                 int height = binaryImage.GetLength(1);
-                Bitmap resultImage = new Bitmap(width, height, PixelFormat.Format24bppRgb);
+                Bitmap resultImage = new Bitmap(
+                    width, height, PixelFormat.Format24bppRgb);
 
-                BitmapData bitmapData = resultImage.LockBits(new Rectangle(0, 0, width, height),
-                                                             ImageLockMode.WriteOnly, resultImage.PixelFormat);
+                BitmapData bitmapData = resultImage.LockBits(
+                    new Rectangle(0, 0, width, height),
+                    ImageLockMode.WriteOnly,
+                    resultImage.PixelFormat);
 
                 int bytesPerPixel = Image.GetPixelFormatSize(resultImage.PixelFormat) / 8;
-                int stride = bitmapData.Stride;
-                IntPtr ptrFirstPixel = bitmapData.Scan0;
+                int stride = bitmapData.Stride;                
                 int byteCount = stride * height;
                 byte[] pixels = new byte[byteCount];
+                IntPtr ptrFirstPixel = bitmapData.Scan0;
 
-                Parallel.For(0, height, new ParallelOptions { MaxDegreeOfParallelism = Threads }, y =>
+                Parallel.For(
+                    0,
+                    height,
+                    new ParallelOptions { MaxDegreeOfParallelism = Threads },
+                    y =>
                 {
                     int yOffset = y * stride;
                     for (int x = 0; x < width; x++)
@@ -105,6 +146,12 @@ namespace MultiThreadedComputingOnCPU
                 return resultImage;
             }            
 
+            /// <summary>
+            /// Выполняет алгоритм эрозии изображения.
+            /// </summary>
+            /// <param name="binaryImage">Бинарное изображение.</param>
+            /// <param name="step">Шаг.</param>
+            /// <returns>Бинарная матрица.</returns>
             private static int[,] Erose(int[,] binaryImage, int step)
             {
                 int width = binaryImage.GetLength(0);
@@ -112,7 +159,11 @@ namespace MultiThreadedComputingOnCPU
                 int[,] intermediateImage = new int[width, height];
                 int[,] erodedImage = new int[width, height];
 
-                Parallel.For(0, height, new ParallelOptions { MaxDegreeOfParallelism = Threads }, y =>
+                Parallel.For(
+                    0,
+                    height,
+                    new ParallelOptions { MaxDegreeOfParallelism = Threads },
+                    y =>
                 {
                     for (int x = 0; x < width; x++)
                     {
@@ -130,7 +181,10 @@ namespace MultiThreadedComputingOnCPU
                     }
                 });
 
-                Parallel.For(0, width, new ParallelOptions { MaxDegreeOfParallelism = Threads }, x =>
+                Parallel.For(0,
+                    width,
+                    new ParallelOptions { MaxDegreeOfParallelism = Threads },
+                    x =>
                 {
                     for (int y = 0; y < height; y++)
                     {
@@ -151,12 +205,18 @@ namespace MultiThreadedComputingOnCPU
                 return erodedImage;
             }            
 
+            /// <summary>
+            /// Применяет фильтр эрозии к изображению.
+            /// </summary>
+            /// <param name="image">Исходное изображение.</param>
+            /// <returns>Изображение с эрозией.</returns>
             public static Bitmap ApplyErossion(Bitmap image)
             {
                 var intensivityMat = CalculateIntensivityOfImage(image);
                 var binaryImage = ApplyThreshold(intensivityMat, 150);
                 var erodedImage = Erose(binaryImage, 1);
-                image.Dispose();
+                image.Dispose();                
+
                 return ConvertBinaryImageToBitmap(erodedImage);
             }
         }
