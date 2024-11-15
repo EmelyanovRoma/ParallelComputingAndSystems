ca43b133d70e5b776970b7535ccce8079291f77d
Romanter456@gmail.com <Romanter456@gmail.com>
2024-11-16 01:12:55 +0700

    feature (Program.cs): добавлен метод для вывода некоторых характеристик установленных в системе GPU.

--- src/MultiThreadedComputingOnGPU/Program.cs
+++ src/MultiThreadedComputingOnGPU/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using OpenCL.Net;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -11,11 +12,10 @@ namespace MultiThreadedComputingOnGPU
     public class Program
     {
         public static void Main(string[] args)
-        {            
-            Bitmap bmp = OpenImage("image1.jpg");
-            SaveImage(bmp);
+        {
+            ShowGPUProperties();
         }
-
+                
         /// <summary>
         /// Сохраняет изображение.
         /// </summary>
@@ -62,6 +62,48 @@ namespace MultiThreadedComputingOnGPU
             }
             
             return img;
+        }
+
+        /// <summary>
+        /// Отображает некоторые характеристики уствновленных в системе GPU.
+        /// </summary>
+        public static void ShowGPUProperties()
+        {
+            ErrorCode error;
+            var platforms = Cl.GetPlatformIDs(out error);
+
+            if (error != ErrorCode.Success)
+            {
+                Console.WriteLine("Не удалось получить платформы.");
+                return;
+            }
+
+            foreach (var platform in platforms)
+            {
+                Console.WriteLine(
+                    $"Платформа: " +
+                    $"{Cl.GetPlatformInfo(platform, PlatformInfo.Name, out _)}");
+
+                var devices = Cl.GetDeviceIDs(platform, DeviceType.Gpu, out error);
+
+                if (error != ErrorCode.Success)
+                {
+                    Console.WriteLine("Не удалось получить устройства на платформе.");
+                    continue;
+                }
+
+                foreach (var device in devices)
+                {
+                    Console.WriteLine("Устройство:");
+                    Console.WriteLine($"  Имя: {Cl.GetDeviceInfo(device, DeviceInfo.Name, out _)}");
+                    Console.WriteLine($"  Производитель: {Cl.GetDeviceInfo(device, DeviceInfo.Vendor, out _)}");
+                    Console.WriteLine($"  Драйвер: {Cl.GetDeviceInfo(device, DeviceInfo.DriverVersion, out _)}");
+                    Console.WriteLine($"  Версия OpenCL: {Cl.GetDeviceInfo(device, DeviceInfo.Version, out _)}");
+                    Console.WriteLine($"  Ядер вычисления: {Cl.GetDeviceInfo(device, DeviceInfo.MaxComputeUnits, out _)}");
+                    Console.WriteLine($"  Глобальная память: {Cl.GetDeviceInfo(device, DeviceInfo.GlobalMemSize, out _)} байт");
+                    Console.WriteLine($"  Частота процессора: {Cl.GetDeviceInfo(device, DeviceInfo.MaxClockFrequency, out _)} MHz");
+                }
+            }
         }
     }
 }
