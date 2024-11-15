b8b1ab7a9d4ca7ffbe06b0c7aa2b9fbeb81c53af
Romanter456@gmail.com <Romanter456@gmail.com>
2024-11-16 00:13:22 +0700

    feature (MultiThreadedComputingOnGPU): добавлен проект для обработки изображений при помощи видеокарты с использованием библиотеки OpenCL.NET

---
+++ src/MultiThreadedComputingOnGPU/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiThreadedComputingOnGPU
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
